e6e6f4c186b698d93d07059d99d51f47759ef6fb
SebastianLandaeta <sebas.landa@hotmail.com>
2023-08-07 16:01:32 -0400

    Base de datos 0.1 ya creada
    Se creó una base de datos basica, la cual claramente esta abierta a cambio, pero que contiene lo que se considera elemental para el proyecto, junto con sus modelos y migraciones, y se incluyó una librería con composer para lo de los codigos qr

---
+++ app/Http/Controllers/EjemploController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+class EjemploController extends Controller
+{
+    //
+}
---
+++ database/migrations/2023_08_07_174116_create_cursosparticipantes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cursosparticipantes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('curso_fk');
+            $table->foreign('curso_fk')->references('id')->on('cursos');
+            $table->unsignedBigInteger('participante_fk');
+            $table->foreign('participante_fk')->references('cedula')->on('participantes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cursosparticipantes');
+    }
+};
